4345ed7c7679eafbaa86b71244709c8568c1014a
lams2135 <lams2135@gmail.com>
2014-08-30 23:57:52 +0800

    fix-bugs-20140830-X1

--- src/mekblog/__init__.py
+++ src/mekblog/__init__.py
@@ -5,8 +5,5 @@ import mekblog.comment
 import mekblog.tag
 import mekblog.security
 import mekblog.notification
-<<<<<<< HEAD
 import mekblog.adminpanel
-=======
 import mekblog.SMTPInterface
->>>>>>> b37404dc65de5db5f43729746a923afb4cbb152c
--- src/mekblog/adminpanel.py
+++ src/mekblog/adminpanel.py
@@ -1,3 +1,4 @@
 import mekblog
 
 # TODO: mainly write a render to build HTML+JS dynamically
+

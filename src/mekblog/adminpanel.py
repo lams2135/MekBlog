c1d69e37c80cfbb5b0211a3ab11d8a3f3725a9af
lams2135 <lams2135@gmail.com>
2014-09-05 23:45:54 +0800

    fix-bugs-20140905-X1

--- src/mekblog/adminpanel.py
+++ src/mekblog/adminpanel.py
@@ -5,7 +5,10 @@ import mekblog
 
 # adminpanel_view = flask.Blueprint(__name__)
 
-def render_child_page():
+def render_child_panel(router):
+	# router is extension name or 'core' or ''
+	# get extension information from mekblog.extension
+	# put them into HTML+JS
 	pass
 
 
--- src/mekblog/extension.py
+++ src/mekblog/extension.py
@@ -4,24 +4,39 @@ import mekblog.notification
 import flask
 import json
 
+ext_lists = []
+
 # interface to mekblog
 
 def init():
 	# load all extensions
-	# exec all initial operation
+	# disable all extensions
 	pass
 
 def load(ext):
+	# get path and extension.json
+	# pkg = json.load()
 	# name check
+	# for x in ext_lists:
+	# 	if x['name'] == pkg['name']
+	# 		raise Error
 	# load extension.json
 	# put in config
-	# register in adminpanel
+	# register in adminpanel ( del
 	# import module
-	# register initial	 
+	# register initial
 	# register view
 	# deal with template & static files
 	pass
 
+def enable(ext):
+	pass
+
+def disable(ext):
+	pass
+
+def unload(ext):
+	pass
 
 # interface to extensions
 
